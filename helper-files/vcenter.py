30ce96c3884c3b95d16aed1dc31fd73cca2981c3
wouyang <36004307+wouyang666@users.noreply.github.com>
2020-11-02 16:38:01 -0500

    Update vcenter.py

--- helper-files/vcenter.py
+++ helper-files/vcenter.py
@@ -1,9 +1,7 @@
 from __future__ import print_function
 from pyVim.connect import SmartConnect, Disconnect
 from pyVmomi import vim
-import argparse
 import atexit
-import getpass
 import ssl
 from junossecure.junos_secure import junos_decode
 
@@ -16,6 +14,13 @@ def GetVMs(content):
     vm_view.Destroy()
     return obj
 
+def GetHosts(content):
+    host_view = content.viewManager.CreateContainerView(content.rootFolder,[vim.HostSystem],True)
+    #obj = [vm for vm in vm_view]
+    obj = [host for host in host_view.view]
+    #vm_view.Destroy()
+    host_view.Destroy()
+    return obj
 
 def run():
 
@@ -41,6 +46,15 @@ def run():
     atexit.register(Disconnect, si)
 
     content = si.RetrieveContent()
+
+    hosts = GetHosts(content)
+    host_list = []
+    for host in hosts:
+        host_name = host.config.network.dnsConfig.hostName
+        host = host.config.host
+        host_list.append({"host": host, "host_name": host_name})
+    #print(host_list)
+
     vms = GetVMs(content)
     vm_list = []
     for vm in vms:
@@ -50,8 +64,14 @@ def run():
         if vm.summary.guest != None:
             ip = vm.summary.guest.ipAddress
             name = vm.summary.config.name
-        #print("Name       : ", name)
-        #print("IP         : ", ip)
+            host = vm.summary.runtime.host
+            ###### match the HostSystem to the Hostname of the physical host
+            for item in host_list:
+              if item["host"] == host:
+                host_name = item["host_name"]
+                break
+            #print("Name       : ", name)
+            #print("IP         : ", ip)
         if vm.config != None:
             #print("########vm config name######")
             #print(vm.config)
@@ -62,8 +82,7 @@ def run():
                 if hasattr(device, 'macAddress'):
                     mac = device.macAddress
                     #print("MAC        : ", mac)
-        vm_list.append({'tags': {"name": name}, "fields": {"ip": ip, "mac": mac}})
-    #for vm in vm_list:
-    #    print(vm)
+        vm_list.append({'tags': {"name": name}, "fields": {"ip": ip, "mac": mac, "host": host_name}})
+    #for vm in vm_list: print(vm)
     return vm_list
 
