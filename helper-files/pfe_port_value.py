affc451d3e829462c92bea0f831925efb4dfad96
wouyang <36004307+wouyang666@users.noreply.github.com>
2020-11-19 10:53:03 -0500

    Update pfe_port_value.py

--- helper-files/pfe_port_value.py
+++ helper-files/pfe_port_value.py
@@ -1,3 +1,4 @@
+#Jay Wilson, Wushu(Wilson) Ouyang
 
 from netmiko import ConnectHandler
 from junossecure.junos_secure import junos_decode
@@ -5,7 +6,7 @@ from junossecure.junos_secure import junos_decode
 def get_junos_command(device, command):
     net_connect = ConnectHandler(**device)
     output = net_connect.send_command(command)
-    net_connect.disconnect() 
+    net_connect.disconnect()
     return output
 
 
@@ -14,16 +15,35 @@ def run():
     host = __pillar__["proxy"]["host"]
     user= __pillar__["proxy"]["username"]
     pwd = junos_decode(__pillar__["proxy"]["encoded_password"])
-    device = { 
-       "device_type": "juniper_junos", 
-       "host": host, 
-       "username": user, 
-       "password": pwd, 
-       } 
-
+    device = {
+       "device_type": "juniper_junos",
+       "host": host,
+       "username": user,
+       "password": pwd,
+       }
+    meaning = {
+        'RDBGC0': '(All Received Drop)',
+        'RDBGC1': '(Filter Block Drop)',
+        'RDBGC2': '(Multicast Drop)',
+        'RDBGC3': '(VLAN Drop)',
+        'RDBGC4': '(Policy Discard Drop)',
+        'RDBGC5': '(Parity Error Drop)',
+        'RDBGC6': '(VLAN Field Processor Drop)',
+        'RDBGC7': '(L2/L3 Lookup DST_DISCARD Drop)',
+        'RDBGC8': '(11 other error Drops)',
+        'TDBGC1': '(IPv6 L3 and IPMC Aged and Drop)',
+        'TDBGC3': '(All Transmit Drop)',
+        'TDBGC5': '(IPv4 L3 and IPMC Aged and Drop)',
+        'TDBGC6': '(L2 Multicast Drop)',
+        'TDBGC7': '(Aged Drop)',
+        'TDBGC8': '(STP not in FWD state Drop)',
+        'TDBGC9': '(VXLT Translate Drop)',
+        'TDBGC10': '(Invalid VLAN Drop)',
+        'TDBGC11': '(6 other error Drops)',
+    }
     command1 = 'request routing-engine execute command "/usr/sbin/cprod -A fpc0 -c \'show dcbc ifd all\' | awk \'NR > 3 {print $4}\' | xargs -I [] /usr/sbin/cprod -A fpc0 -c \'set dcbc bc \\"show c []\\"\' | awk \'/RDBGC0/ {print $0}; /TDBGC/ {print $0}\'"'
     command2 = 'request routing-engine execute command "/usr/sbin/cprod -A fpc0 -c \'show dcbc ifd all\'"'
-    
+
 
     output1 = get_junos_command(device,command1)
     #print(result)
@@ -33,21 +53,22 @@ def run():
         if line:
             line = line.split()
             value = line[3]
-            value = value[1:]
+            value = value[1:].replace(",", "")
             value = int(value)
             #print("value", value)
-            key = line[0]       
+            key = line[0]
             name = line[0].split(".")[0]
             #print("name", name)
             port = line[0].split(".")[1]
             #print("port", port)
-            output1_list.append({"tags": {"key": key}, "fields": {"name": name, "port":port, "value":value}})
+            why = meaning.get(name)
+            output1_list.append({"tags": {"key": key}, "fields": {"name": name, "port":port, "value":value, "why":why}})
 
 
     output2 = get_junos_command(device,command2)
     output2 = output2.split("\n")
     #remove the first 3 header lines
-    output2_new = output2[4:] 
+    output2_new = output2[4:]
 
     ifd_port_list = []
     for item in output2_new:
